41c72b5280866b521a41d2535d8a4204697fa90e
edmondkipkorir <eddykarev@gmail.com>
2022-03-07 21:51:36 +0300

    changed the background colour

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
->let form =document.getElementById(form)
+let form =document.getElementById("form");
+let yearOfBirth =document.getElementById("year");
+let monthOfBirth =document.getElementById("month");
+let dateOfBirth =document.getElementById("date");
+let myResult =document.querySelector("result");
+
+form.addEventListener("submit")
