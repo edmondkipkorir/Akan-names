1fff75a6dbbcd0f70fc9e8ca43827ca8c99f1c90
edmondkipkorir <eddykarev@gmail.com>
2022-03-08 16:01:13 +0300

    added formula

--- index.js
+++ index.js
@@ -8,4 +8,14 @@ function dispayName() {
     var year = document.getElementById("year").value;
     var yy = parseInt(year);
     var cc = (yy - 1) / 100 + 1; 
+    var bornDay = parseInt(cc / 4 - 2 * cc - 1 + (5 * yy) / 4 + (26 * (mm + 1)) / 10 + dd) % 7;
+    if(document.getElementById("gender").checked){
+        var gender="male"
+    }else{
+        var gender="female"
+    }
+    if (dd <= 0 || dd == "" || dd > 31) {
+        alert("invalid Date");
+    } else if (mm <= 0 || mm > 12 || mm == "" || mm == 2 && dd > 29) {
+        alert("invalid Month");
 }
